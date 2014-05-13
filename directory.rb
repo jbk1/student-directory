af329654874a5ec18bc018c020540e516224c4c0
James B Kemp <james@bibble.com>
2014-05-13 13:23:55 +0100

    refactored plus array

--- directory.rb
+++ directory.rb
@@ -1,28 +1,30 @@
 # first we add the list of students
-puts "The students on my cohort of Makers Academy"
-puts "James Kemp"
-puts "Nic Yeeles"
-puts "Roi Driscoll"
-puts "James Whyte"
-puts "Margherita Serino"
-puts "Vaidas Mykolaitis"
-puts "Johann Bornman"
-puts "Kate Hamilton"
-puts "James Kemp"
-puts "Nic Yeeles"
-puts "Julie Walker"
-puts "Will Allen"
-puts "Julia Tan"
-puts "Federico Maffei"
-puts "Jamie Patel"
-puts "Faezrah Rizalman"
-puts "Josh Fail-Broon"
-puts "Sasha Cooper"
-puts "Nicolai DTH"
-puts "Nadav Matalon"
-puts "Fitsum Teklehaimanot"
+students [
+"The students on my cohort of Makers Academy",
+"James Kemp",
+"Nic Yeeles",
+"Roi Driscoll",
+"James Whyte",
+"Margherita Serino",
+"Vaidas Mykolaitis",
+"Johann Bornman",
+"Kate Hamilton",
+"James Kemp",
+"Nic Yeeles",
+"Julie Walker",
+"Will Allen",
+"Julia Tan",
+"Federico Maffei",
+"Jamie Patel",
+"Faezrah Rizalman",
+"Josh Fail-Broon",
+"Sasha Cooper",
+"Nicolai DTH",
+"Nadav Matalon",
+"Fitsum Teklehaimanot"]
+
+students.each do |student|
+	puts student
+
 #finally, we print the total
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 21
-puts " great students"
+print "Overall, we have #{students.length} great students"
