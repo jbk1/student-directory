7a615c5b3df1441c1f3ebd2e22e69cc1f70b79f2
James B Kemp <james@bibble.com>
2014-05-14 16:44:08 +0100

    added hashes

--- directory.rb
+++ directory.rb
@@ -1,25 +1,25 @@
 # first we add the list of students
 students = [
-["James Kemp", :may]
-["Nic Yeeles", :may]
-["Roi Driscoll", :may]
-["James Whyte", :may]
-["Margherita Serino", :may]
-["Vaidas Mykolaitis", :may]
-["Johann Bornman", :may]
-["Kate Hamilton", :may]
-["James Kemp", :may]
-["Nic Yeeles", :may]
-["Julie Walker", :may]
-["Will Allen", :may]
-["Julia Tan", :may]
-["Federico Maffei", :may]
-["Jamie Patel", :may]
-["Faezrah Rizalman", :may]
-["Josh Fail-Broon", :may]
-["Sasha Cooper", :may]
-["Nicolai DTH", :may]
-["Nadav Matalon", :may]
+{name => "James Kemp", :cohort => :may},
+{name => "Nic Yeeles", :cohort => :may},
+{name => "Roi Driscoll", :cohort => :may},
+{name => "James Whyte", :cohort => :may},
+{name => "Margherita Serino", :cohort => :may},
+{name => "Vaidas Mykolaitis", :cohort => :may},
+{name => "Johann Bornman", :cohort => :may},
+{name "Kate Hamilton", :may],
+["James Kemp", :may],
+["Nic Yeeles", :may],
+["Julie Walker", :may],
+["Will Allen", :may],
+["Julia Tan", :may],
+["Federico Maffei", :may],
+["Jamie Patel", :may],
+["Faezrah Rizalman", :may],
+["Josh Fail-Broon", :may],
+["Sasha Cooper", :may],
+["Nicolai DTH", :may],
+["Nadav Matalon", :may],
 ["Fitsum Teklehaimanot", :may]
 ]
 
