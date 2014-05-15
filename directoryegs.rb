969696114e348d3474e30ed5f74d38f040aee79d
James B Kemp <james@bibble.com>
2014-05-15 18:36:23 +0100

    Tidy completed Q8

--- directoryegs.rb
+++ directoryegs.rb
@@ -1,42 +1,36 @@
+def get_user_age
+	puts "Age, please: "
+	age = gets.chomp
+	return "N/A" if age.empty?
+	return age
+end
 
+def get_user_cohort
+	puts "Cohort please: "
+	cohort = gets.chomp
+	return "N/A" if cohort.empty?
+	return cohort
+end
+
+def ask_user_name
+	puts "Please enter the name of the student"
+	user_input = gets.chomp
+end
 
 
 def input_students
-	puts "Please enter the name of the students"
-	puts "To finish just hit return twice"
-	#create an empty array
 	students = []
-	#get the first name
-	name = gets.chomp
-	#while the code is not empty repeat this code
+	name = ask_user_name
 	while !name.empty? do
-		puts "Pls type their age:"
-		age = gets.chomp
-		# re-asking the question if they leave it empty
-		while age.empty? do
-			puts "their age, please?:"
-			age = gets.chomp
-		end
-		puts "Pls type their cohort"
-		cohort = gets.chomp
-			# re-asking the question if they leave it empty
-		while cohort.empty? do
-			puts "their cohort, please?:"
-			cohort = gets.chomp
-		end
-		# add the student hash to the array
-		students << {:name => name, :age => age, :cohort => cohort}
-		puts "Now we have #{students.length} students"
+		students << {:name => name, :age => get_user_age, :cohort => get_user_cohort}
+		puts "Now we have #{students.count} students"
 		#get another name from the user
-		name = gets.chomp
+		name = ask_user_name
 	end
 	# return the array of students
 	students
 	#print 'enter age question'
-
 end
-
-
 
 
 def print_header
@@ -47,7 +41,7 @@ end
 def print(names)
 	counter = 0
 	while counter < names.length
-		puts "#{names[counter][:name]}".center(20) + "(cohort: #{names[counter][:cohort]})".center(20) + "(age: #{names[counter][:age]})".center(20)
+		puts "#{names[counter][:name].center(20, "-")}  (age: #{names[counter][:age].center(20, "-")}) (cohort: #{names[counter][:cohort].center(20, "-")})"
 		counter += 1
 	end
 end
